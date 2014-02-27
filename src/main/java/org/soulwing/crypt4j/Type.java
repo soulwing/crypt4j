1af477386af5c1d3220fbea488076883ef3d457e
Carl Harris <ceharris@vt.edu>
2014-02-27 10:42:15 -0500

    file header cleanup

--- src/main/java/org/soulwing/crypt4j/Type.java
+++ src/main/java/org/soulwing/crypt4j/Type.java
@@ -1,8 +1,31 @@
+/*
+ * File created on Feb 27, 2014 
+ *
+ * Copyright (c) 2014 Carl Harris, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package org.soulwing.crypt4j;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * A enumeration of supported encryption types.
+ *
+ * @author Carl Harris
+ */
 enum Type {
   
   SHA256(5, "SHA-256", Sha256Crypt.class),
